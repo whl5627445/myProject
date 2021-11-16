538be60345330c5a2fa746bcd9e78431b71764da
simtek/wnaghailong <Whl5627445>
2021-11-16 09:25:15 +0800

    开发版本, 添加utf8声明，防止出现不兼容字符

--- config/DB_config.py
+++ config/DB_config.py
@@ -1,21 +1,22 @@
+# -- coding: utf-8 --
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, close_all_sessions
 
 # 本地
-engine = create_engine('mysql+pymysql://root:root@localhost:3306/test?charset=utf8mb4',
-                       pool_size=200,
-                       max_overflow=50,
-                       pool_recycle=3600,
-                       # pool_timeout=15,
-                       pool_pre_ping=True,
-                       echo_pool=True,
-                       )
-# 测试环境
-# engine = create_engine('mysql+pymysql://root:root@localhost:3666/test?charset=utf8mb4',
+# engine = create_engine('mysql+pymysql://root:root@localhost:3306/test?charset=utf8mb4',
 #                        pool_size=200,
-#                        max_overflow=500,
+#                        max_overflow=50,
 #                        pool_recycle=3600,
+#                        # pool_timeout=15,
 #                        pool_pre_ping=True,
+#                        echo_pool=True,
 #                        )
+# 测试环境
+engine = create_engine('mysql+pymysql://root:root@localhost:3666/test?charset=utf8mb4',
+                       pool_size=200,
+                       max_overflow=500,
+                       pool_recycle=3600,
+                       pool_pre_ping=True,
+                       )
 DBSession = sessionmaker(bind=engine, autoflush=False, autocommit=True)
 session = DBSession()
