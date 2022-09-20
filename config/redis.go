eecb3aa1796c970ad924feaf8c5db0b88fbd8c4b
simtek/wanghailong <Whl5627445>
2022-09-20 17:07:01 +0800

    redis开发地址

--- config/redis.go
+++ config/redis.go
@@ -4,10 +4,15 @@ import (
 	"github.com/go-redis/redis/v8"
 )
 
+var addr = "redis:6379"
+
 func openRedis() *redis.Client {
+	if DEBUG != "" {
+		addr = "192.168.121.150:6379"
+	}
 	rdb := redis.NewClient(&redis.Options{
 		//Addr:     "119.3.155.11:6379",
-		Addr:     "redis:6379",
+		Addr:     addr,
 		Password: "", // no password set
 		DB:       0,  // use default DB
 	})
