9b03c4ad3dedc0cd09f75e42400bf56f12d905bd
xuqingda <qingda.xu@cnydsimtek.com>
2023-05-09 16:23:19 +0800

    时间转换：输入秒，输出"月 天 小 时 分 秒"的格式。

--- library/timeConvert/timeFormat.go
+++ library/timeConvert/timeFormat.go
@@ -1,27 +1,34 @@
 package timeConvert
 
 import (
+	"fmt"
 	"log"
-	"strconv"
 	"time"
 )
 
 func secondToTimeString(simulateSecond int) string {
-	hours := simulateSecond / 3600
+	months := simulateSecond / (86400 * 30)
+	days := simulateSecond % (86400 * 30) / 86400
+	hours := (simulateSecond % 86400) / 3600
 	minutes := simulateSecond % 3600 / 60
 	seconds := simulateSecond % 60
 	if simulateSecond < 0 {
 		log.Println("仿真运行时间为负： ", simulateSecond)
 		return "1秒"
 	}
-	if hours == 0 {
-		if minutes == 0 {
-			return strconv.Itoa(seconds) + "秒"
-		} else {
-			return strconv.Itoa(minutes) + "分钟" + strconv.Itoa(seconds) + "秒"
-		}
+	if months > 0 {
+
+	}
+	if months > 0 {
+		return fmt.Sprintf("%d月%d天%d小时%d分钟%d秒", months, days, hours, minutes, seconds)
+	} else if days > 0 {
+		return fmt.Sprintf("%d天%d小时%d分钟%d秒", days, hours, minutes, seconds)
+	} else if hours > 0 {
+		return fmt.Sprintf("%d小时%d分钟%d秒", hours, minutes, seconds)
+	} else if minutes > 0 {
+		return fmt.Sprintf("%d分钟%d秒", minutes, seconds)
 	} else {
-		return strconv.Itoa(hours) + "小时" + strconv.Itoa(minutes) + "分钟" + strconv.Itoa(seconds) + "秒"
+		return fmt.Sprintf("%d秒", seconds)
 	}
 }
 
