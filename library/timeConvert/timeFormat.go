aef4d58e0665b0c077ed5fb3a21ca2d2b4121513
simtek/wanghailong <851941017@qq.com>
2023-05-17 17:59:46 +0800

    1.时间格式化函数新增支持按时间单位截断

--- library/timeConvert/timeFormat.go
+++ library/timeConvert/timeFormat.go
@@ -3,10 +3,11 @@ package timeConvert
 import (
 	"fmt"
 	"log"
+	"strings"
 	"time"
 )
 
-func secondToTimeString(simulateSecond int) string {
+func secondToTimeString(simulateSecond, num int) string {
 	months := simulateSecond / (86400 * 30)
 	days := simulateSecond % (86400 * 30) / 86400
 	hours := (simulateSecond % 86400) / 3600
@@ -16,25 +17,38 @@ func secondToTimeString(simulateSecond int) string {
 		log.Println("仿真运行时间为负： ", simulateSecond)
 		return "1秒"
 	}
-	if months > 0 {
 
-	}
+	timeStr := ""
 	if months > 0 {
-		return fmt.Sprintf("%d月%d天%d小时%d分钟%d秒", months, days, hours, minutes, seconds)
+		timeStr = fmt.Sprintf("%d月,%d天,%d小时,%d分钟,%d秒", months, days, hours, minutes, seconds)
 	} else if days > 0 {
-		return fmt.Sprintf("%d天%d小时%d分钟%d秒", days, hours, minutes, seconds)
+		timeStr = fmt.Sprintf("%d天,%d小时,%d分钟,%d秒", days, hours, minutes, seconds)
 	} else if hours > 0 {
-		return fmt.Sprintf("%d小时%d分钟%d秒", hours, minutes, seconds)
+		timeStr = fmt.Sprintf("%d小时,%d分钟,%d秒", hours, minutes, seconds)
 	} else if minutes > 0 {
-		return fmt.Sprintf("%d分钟%d秒", minutes, seconds)
+		timeStr = fmt.Sprintf("%d分钟,%d秒", minutes, seconds)
 	} else {
-		return fmt.Sprintf("%d秒", seconds)
+		timeStr = fmt.Sprintf("%d秒", seconds)
 	}
+	if num != -1 {
+		strList := strings.Split(timeStr, ",")
+		if len(strList) >= num {
+			return strings.Join(strList[:num], "")
+		}
+	}
+	return strings.ReplaceAll(timeStr, ",", "")
 }
 
 func UseTimeFormat(startTime, endTime int) string {
 	if endTime <= 0 {
 		endTime = int(time.Now().Unix())
 	}
-	return secondToTimeString(endTime - startTime)
+	return secondToTimeString(endTime-startTime, -1)
+}
+
+func UseTimeFormatNew(startTime, endTime, num int) string {
+	if endTime <= 0 {
+		endTime = int(time.Now().Unix())
+	}
+	return secondToTimeString(endTime-startTime, num)
 }
