1750ba82073e8bedbb57f0700113810880d0d2f2
simtek/wanghailong <851941017@qq.com>
2023-06-05 15:06:05 +0800

    1.优化GC触发条件

--- Init/GC_GcollectAndUnmap.go
+++ Init/GC_GcollectAndUnmap.go
@@ -9,7 +9,9 @@ func GcCollectAndUnmap() {
 	time.Sleep(60 * time.Second)
 	for {
 		// 每200秒强制执行GC一次
-		omc.OMC.SendExpressionNoParsed("GC_gcollect_and_unmap()")
+		if omc.OMCInstance.Start {
+			omc.OMC.SendExpressionNoParsed("GC_gcollect_and_unmap()")
+		}
 		time.Sleep(time.Second * 200)
 	}
 }
