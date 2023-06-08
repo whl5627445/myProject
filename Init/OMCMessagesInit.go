eaa92402f35447ab18bf0172bcb64eed55dd948a
simtek/wanghailong <851941017@qq.com>
2023-06-08 19:42:55 +0800

    消息推送线程增加omc实例锁

--- Init/OMCMessagesInit.go
+++ Init/OMCMessagesInit.go
@@ -11,11 +11,13 @@ func OMCMessagesInit() {
 	log.Println("初始化消息通知服务")
 	for {
 		time.Sleep(time.Second * 5)
-		if omc.OMCInstance.Start {
+		if omc.OMCInstance.Start && omc.OMCInstance.Cmd != nil {
+			omc.OMCInstance.Mu.Lock()
 			messageList := service.GetMessagesStringInternal()
 			for _, mes := range messageList {
 				_ = service.MessageNotice(mes)
 			}
+			omc.OMCInstance.Mu.Unlock()
 		}
 	}
 }
