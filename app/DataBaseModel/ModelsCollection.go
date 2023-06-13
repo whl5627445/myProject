ca5d99c54f3d6fe3f25a4d3008fcbb98499f031a
simtek/wanghailong <851941017@qq.com>
2023-06-13 20:09:19 +0800

    收藏模型表字段属性变更

--- app/DataBaseModel/ModelsCollection.go
+++ app/DataBaseModel/ModelsCollection.go
@@ -9,9 +9,9 @@ import (
 type YssimModelsCollection struct {
 	ID          string         `gorm:"column:id;primaryKey;type:varchar(128)"`
 	PackageId   string         `gorm:"column:package_id;type:varchar(128)"`
-	ModelName   string         `gorm:"column:model_name;type:varchar(32)"`
+	ModelName   string         `gorm:"column:model_name;type:varchar(128)"`
 	CreatedAt   *time.Time     `gorm:"column:create_time;autoCreateTime"`
 	UpdatedAt   *time.Time     `gorm:"column:update_time"`
-	UserSpaceId string         `gorm:"column:userspace_id;default:\"0\";type:varchar(32)"`
+	UserSpaceId string         `gorm:"column:userspace_id;default:\"0\";type:varchar(128)"`
 	Deleted     gorm.DeletedAt `gorm:"column:deleted_at"`
 }
