1c6f57aa2480185b9db9c4480818c08cac9a3c08
simtek/wanghailong <851941017@qq.com>
2023-07-25 16:21:09 +0800

    上传文件为zip文件时，将GBK转换为utf8

--- library/fileOperation/operation.go
+++ library/fileOperation/operation.go
@@ -1,6 +1,7 @@
 package fileOperation
 
 import (
+	"bytes"
 	"container/list"
 	"errors"
 	"io"
@@ -10,7 +11,10 @@ import (
 	"path/filepath"
 	"strings"
 
+	"github.com/klauspost/compress/zip"
 	"github.com/mholt/archiver/v3"
+	"golang.org/x/text/encoding/simplifiedchinese"
+	"golang.org/x/text/transform"
 )
 
 // 判断所给路径文件/文件夹是否存在
@@ -123,10 +127,63 @@ func WriteFileByte(fileName string, data []byte) bool {
 }
 
 func UnZip(filePath string, toPath string) error {
-	err := archiver.Unarchive(filePath, toPath)
+	err := errors.New("")
+	if strings.HasSuffix(filePath, ".zip") {
+		err = unZip(filePath, toPath)
+	} else {
+		err = archiver.Unarchive(filePath, toPath)
+	}
 	if err != nil {
 		log.Println(err)
 		return err
+	}
+	return nil
+}
+
+func unZip(zipFile string, destDir string) error {
+	zipReader, err := zip.OpenReader(zipFile)
+	if err != nil {
+		return err
+	}
+	defer zipReader.Close()
+	var decodeName string
+	for _, f := range zipReader.File {
+		if f.Flags == 0 {
+			//如果标致位是0  则是默认的本地编码   默认为gbk
+			i := bytes.NewReader([]byte(f.Name))
+			decoder := transform.NewReader(i, simplifiedchinese.GBK.NewDecoder())
+			content, _ := ioutil.ReadAll(decoder)
+			decodeName = string(content)
+		} else {
+			//如果标志为是 1 << 11也就是 2048  则是utf-8编码
+			decodeName = f.Name
+		}
+
+		fpath := filepath.Join(destDir, decodeName)
+		if f.FileInfo().IsDir() {
+			os.MkdirAll(fpath, os.ModePerm)
+		} else {
+			if err = os.MkdirAll(filepath.Dir(fpath), os.ModePerm); err != nil {
+				return err
+			}
+
+			inFile, err := f.Open()
+			if err != nil {
+				return err
+			}
+			defer inFile.Close()
+
+			outFile, err := os.OpenFile(fpath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+			if err != nil {
+				return err
+			}
+			defer outFile.Close()
+
+			_, err = io.Copy(outFile, inFile)
+			if err != nil {
+				return err
+			}
+		}
 	}
 	return nil
 }
