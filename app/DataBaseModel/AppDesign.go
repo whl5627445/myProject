e50c26a5d36641769d6fcee04854892717223a4f
xuqingda <qingda.xu@cnydsimtek.com>
2023-08-02 14:33:36 +0800

    数据库表增加一些注释

--- app/DataBaseModel/AppDesign.go
+++ app/DataBaseModel/AppDesign.go
@@ -66,21 +66,21 @@ type AppPage struct {
 	BackgroundColor        string         `gorm:"column:background_color;type:varchar(32);comment:页面背景颜色"`
 	Release                bool           `gorm:"column:is_release;type:bool;comment:页面是否发布"`
 	IsMulSimulate          bool           `gorm:"column:is_mul_simulate;type:bool;comment:是否进行过多轮仿真"`
-	IsPreview              bool           `gorm:"column:is_preview;type:bool"`
-	MulResultPath          string         `gorm:"column:mul_result_path;type:varchar(128)"`
-	MulSimulateState       int            `gorm:"column:mul_sim_state;type:int;default:0"`
-	MulSimulateMessageRead bool           `gorm:"column:mul_sim_message_read;type:bool;default:true"`
-	MulSimulateErr         string         `gorm:"column:mul_sim_err;type:longtext"`
-	MulSimulateTime        int            `gorm:"column:mul_sim_time;type:int"`
-	ReleaseMessageRead     bool           `gorm:"column:release_message_read;type:bool;default:true"`
-	ReleaseErr             string         `gorm:"column:release_err;type:longtext"`
-	ReleaseState           int            `gorm:"column:release_state;type:int;default:0"`
-	ReleaseTime            int            `gorm:"column:release_time;type:int"`
-	NamingOrder            datatypes.JSON `gorm:"column:naming_order;type:json"`
-	AlignmentLine          datatypes.JSON `gorm:"column:alignment_line;type:json"`
-	CreatedAt              *time.Time     `gorm:"column:create_time;autoCreateTime"`
-	UpdatedAt              *time.Time     `gorm:"column:update_time"`
-	Deleted                gorm.DeletedAt `gorm:"column:deleted_at"`
+	IsPreview              bool           `gorm:"column:is_preview;type:bool;comment:是否可以预览"`
+	MulResultPath          string         `gorm:"column:mul_result_path;type:varchar(128);comment:多轮仿真结果路径"`
+	MulSimulateState       int            `gorm:"column:mul_sim_state;type:int;default:0;comment:多轮仿真状态 1初始化 2运行 3失败 4成功 "`
+	MulSimulateMessageRead bool           `gorm:"column:mul_sim_message_read;type:bool;default:true;comment:多轮仿真日志信息是否读取"`
+	MulSimulateErr         string         `gorm:"column:mul_sim_err;type:longtext;comment:多轮仿真错误信息"`
+	MulSimulateTime        int            `gorm:"column:mul_sim_time;type:int;comment:多轮仿真结束时的时间"`
+	//ReleaseMessageRead     bool           `gorm:"column:release_message_read;type:bool;default:true"`
+	//ReleaseErr             string         `gorm:"column:release_err;type:longtext"`
+	//ReleaseState           int            `gorm:"column:release_state;type:int;default:0"`
+	ReleaseTime   int            `gorm:"column:release_time;type:int;comment:发布时间"`
+	NamingOrder   datatypes.JSON `gorm:"column:naming_order;type:json;comment:多轮仿真的文件名的命名顺序"`
+	AlignmentLine datatypes.JSON `gorm:"column:alignment_line;type:json"`
+	CreatedAt     *time.Time     `gorm:"column:create_time;autoCreateTime"`
+	UpdatedAt     *time.Time     `gorm:"column:update_time"`
+	Deleted       gorm.DeletedAt `gorm:"column:deleted_at"`
 }
 
 type AppComponentBases struct {
@@ -104,18 +104,18 @@ type AppComponentBases struct {
 }
 
 type AppPageComponent struct {
-	ID                 string         `gorm:"primaryKey;type:varchar(128)"`
-	PageId             string         `gorm:"column:page_id;type:varchar(128)"`
-	InputName          string         `gorm:"column:input_name;type:varchar(32)"`
-	Output             datatypes.JSON `gorm:"column:output;type:json"`
-	Max                float64        `gorm:"column:max;type:float"`
-	Min                float64        `gorm:"column:min;type:float"`
-	Interval           float64        `gorm:"column:interval;type:float"`
-	Type               string         `gorm:"column:type;type:varchar(32)"`
-	Width              int            `gorm:"column:width;type:int"`
-	Height             int            `gorm:"column:height;type:int"`
-	PositionX          int            `gorm:"column:position_x;type:int"`
-	PositionY          int            `gorm:"column:position_y;type:int"`
+	ID                 string         `gorm:"primaryKey;type:varchar(128);comment:组件唯一标识"`
+	PageId             string         `gorm:"column:page_id;type:varchar(128);comment:页面id"`
+	InputName          string         `gorm:"column:input_name;type:varchar(32);comment:输入变量名"`
+	Output             datatypes.JSON `gorm:"column:output;type:json;comment:输出变量名"`
+	Max                float64        `gorm:"column:max;type:float;comment:最大值"`
+	Min                float64        `gorm:"column:min;type:float;comment:最小值"`
+	Interval           float64        `gorm:"column:interval;type:float;comment:间隔"`
+	Type               string         `gorm:"column:type;type:varchar(32);comment:组件类型"`
+	Width              int            `gorm:"column:width;type:int;comment:组件宽度"`
+	Height             int            `gorm:"column:height;type:int;comment:组件高度"`
+	PositionX          int            `gorm:"column:position_x;type:int;comment:组件x轴相对位置"`
+	PositionY          int            `gorm:"column:position_y;type:int;comment:组件y轴相对位置"`
 	Angle              int            `gorm:"column:angle;type:int"`
 	HorizontalFlip     bool           `gorm:"column:horizontal_flip;type:bool"`
 	VerticalFlip       bool           `gorm:"column:vertical_flip;type:bool"`
@@ -135,19 +135,19 @@ type AppPageComponent struct {
 	IsGroup            bool           `gorm:"column:is_group;type:bool"`
 }
 
-type AppPageComponentsPreview struct {
-	ID                 string         `gorm:"primaryKey;type:varchar(128)"`
-	PageId             string         `gorm:"column:page_id;type:varchar(128)"`
-	InputName          string         `gorm:"column:input_name;type:varchar(32)"`
-	Output             datatypes.JSON `gorm:"column:output;type:json"`
-	Max                float64        `gorm:"column:max;type:float"`
-	Min                float64        `gorm:"column:min;type:float"`
-	Interval           float64        `gorm:"column:interval;type:float"`
-	Type               string         `gorm:"column:type;type:varchar(32)"`
-	Width              int            `gorm:"column:width;type:int"`
-	Height             int            `gorm:"column:height;type:int"`
-	PositionX          int            `gorm:"column:position_x;type:int"`
-	PositionY          int            `gorm:"column:position_y;type:int"`
+type AppPageComponentsPreview struct { //多轮仿真完copy一次组件
+	ID                 string         `gorm:"primaryKey;type:varchar(128);comment:组件唯一标识"`
+	PageId             string         `gorm:"column:page_id;type:varchar(128);comment:页面id"`
+	InputName          string         `gorm:"column:input_name;type:varchar(32);comment:输入变量名"`
+	Output             datatypes.JSON `gorm:"column:output;type:json;comment:输出变量名"`
+	Max                float64        `gorm:"column:max;type:float;comment:最大值"`
+	Min                float64        `gorm:"column:min;type:float;comment:最小值"`
+	Interval           float64        `gorm:"column:interval;type:float;comment:间隔"`
+	Type               string         `gorm:"column:type;type:varchar(32);comment:组件类型"`
+	Width              int            `gorm:"column:width;type:int;comment:组件宽度"`
+	Height             int            `gorm:"column:height;type:int;comment:组件高度"`
+	PositionX          int            `gorm:"column:position_x;type:int;comment:组件x轴相对位置"`
+	PositionY          int            `gorm:"column:position_y;type:int;comment:组件y轴相对位置"`
 	Angle              int            `gorm:"column:angle;type:int"`
 	HorizontalFlip     bool           `gorm:"column:horizontal_flip;type:bool"`
 	VerticalFlip       bool           `gorm:"column:vertical_flip;type:bool"`
@@ -165,19 +165,19 @@ type AppPageComponentsPreview struct {
 	IsGroup            bool           `gorm:"column:is_group;type:bool"`
 }
 
-type AppPageComponentsRelease struct {
-	ID                 string         `gorm:"primaryKey;type:varchar(128)"`
-	PageId             string         `gorm:"column:page_id;type:varchar(128)"`
-	InputName          string         `gorm:"column:input_name;type:varchar(32)"`
-	Output             datatypes.JSON `gorm:"column:output;type:json"`
-	Max                float64        `gorm:"column:max;type:float"`
-	Min                float64        `gorm:"column:min;type:float"`
-	Interval           float64        `gorm:"column:interval;type:float"`
-	Type               string         `gorm:"column:type;type:varchar(32)"`
-	Width              int            `gorm:"column:width;type:int"`
-	Height             int            `gorm:"column:height;type:int"`
-	PositionX          int            `gorm:"column:position_x;type:int"`
-	PositionY          int            `gorm:"column:position_y;type:int"`
+type AppPageComponentsRelease struct { // 发布的时候copy一次组件
+	ID                 string         `gorm:"primaryKey;type:varchar(128);comment:组件唯一标识"`
+	PageId             string         `gorm:"column:page_id;type:varchar(128);comment:页面id"`
+	InputName          string         `gorm:"column:input_name;type:varchar(32);comment:输入变量名"`
+	Output             datatypes.JSON `gorm:"column:output;type:json;comment:输出变量名"`
+	Max                float64        `gorm:"column:max;type:float;comment:最大值"`
+	Min                float64        `gorm:"column:min;type:float;comment:最小值"`
+	Interval           float64        `gorm:"column:interval;type:float;comment:间隔"`
+	Type               string         `gorm:"column:type;type:varchar(32);comment:组件类型"`
+	Width              int            `gorm:"column:width;type:int;comment:组件宽度"`
+	Height             int            `gorm:"column:height;type:int;comment:组件高度"`
+	PositionX          int            `gorm:"column:position_x;type:int;comment:组件x轴相对位置"`
+	PositionY          int            `gorm:"column:position_y;type:int;comment:组件y轴相对位置"`
 	Angle              int            `gorm:"column:angle;type:int"`
 	HorizontalFlip     bool           `gorm:"column:horizontal_flip;type:bool"`
 	VerticalFlip       bool           `gorm:"column:vertical_flip;type:bool"`
--- app/DataBaseModel/Snapshot.go
+++ app/DataBaseModel/Snapshot.go
@@ -8,18 +8,18 @@ import (
 )
 
 type YssimSnapshots struct {
-	ID                string         `gorm:"primaryKey;type:varchar(128)"`
-	SpaceId           string         `gorm:"column:space_id;type:varchar(128)"`
-	PackageId         string         `gorm:"column:package_id;type:varchar(128)"`
-	ExperimentId      string         `gorm:"column:experiment_id;type:varchar(128)"`
-	SnapshotName      string         `gorm:"column:snapshot_name;type:varchar(32)"`
-	UserName          string         `gorm:"column:username;type:varchar(32)"`
-	ModelName         string         `gorm:"column:model_name;type:varchar(128)"`
-	ComponentName     string         `gorm:"column:component_name;type:varchar(128)"`
-	SimulateResultId  string         `gorm:"column:simulate_result_id;type:varchar(128)"`
-	SimulateResultObj datatypes.JSON `gorm:"column:simulate_result_obj;type:json"`
-	SimulateVarData   datatypes.JSON `gorm:"column:simulate_var_data;type:json"`
-	ModelVarData      datatypes.JSON `gorm:"column:model_var_data;type:json"`
+	ID                string         `gorm:"primaryKey;type:varchar(128);comment:视图快照的唯一标识"`
+	SpaceId           string         `gorm:"column:space_id;type:varchar(128);comment:空间id"`
+	PackageId         string         `gorm:"column:package_id;type:varchar(128);comment:包id"`
+	ExperimentId      string         `gorm:"column:experiment_id;type:varchar(128);comment:实验id"`
+	SnapshotName      string         `gorm:"column:snapshot_name;type:varchar(32);comment:快照名称"`
+	UserName          string         `gorm:"column:username;type:varchar(32);comment:用户名"`
+	ModelName         string         `gorm:"column:model_name;type:varchar(128);comment:模型名"`
+	ComponentName     string         `gorm:"column:component_name;type:varchar(128);comment:组件名"`
+	SimulateResultId  string         `gorm:"column:simulate_result_id;type:varchar(128);comment:实验结果id"`
+	SimulateResultObj datatypes.JSON `gorm:"column:simulate_result_obj;type:json;comment:视图的仿真结果数据"`
+	SimulateVarData   datatypes.JSON `gorm:"column:simulate_var_data;type:json;comment:仿真参数数据"`
+	ModelVarData      datatypes.JSON `gorm:"column:model_var_data;type:json;comment:模型参数数据"`
 
 	CreatedAt *time.Time     `gorm:"column:create_time;autoCreateTime"`
 	UpdatedAt *time.Time     `gorm:"column:update_time"`
