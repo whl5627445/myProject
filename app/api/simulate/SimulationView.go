5df7449e6e5214a7791badf770dfe507cf7a0648
xuqingda <qingda.xu@cnydsimtek.com>
2023-08-14 13:28:55 +0800

    /simulation/result/singular结果长度超过1000筛选优化。

--- app/api/simulate/SimulationView.go
+++ app/api/simulate/SimulationView.go
@@ -271,22 +271,19 @@ func SimulateResultSingularView(c *gin.Context) {
 			if unitsData[0] == "true" {
 				scaleFactor, _ := strconv.ParseFloat(unitsData[1], 64)
 				offset, _ := strconv.ParseFloat(unitsData[2], 64)
-				if len(ordinate) > 1000 {
+				length := len(ordinate)
+				if length > 1000 {
 					step := len(ordinate) / 1000
 					o := []float64{}
 					a := []float64{}
 					for s := 0; s < len(ordinate); s++ {
 						index := s * step
-						if index > 1000 {
+						if index > length || len(o) > 999 {
 							break
 						}
 						o = append(o, data[1][index])
 						a = append(a, data[0][index])
 					}
-					//if len(ordinate)%1000 != 0 {
-					//	o = append(o, data[1][len(ordinate)-1])
-					//	a = append(a, data[0][len(ordinate)-1])
-					//}
 					ordinate = o
 					abscissa = a
 				}
