5d07bef59ecbed891bd86c8322889456dd3f1c95
zhouqiang <qiang.zhou@simtek.cc>
2024-04-25 16:25:40 +0800

    修复bug: 点击模型检查，日志在模型检查没通过时会多显示一行空白

--- app/v1/service/ModelCheck.go
+++ app/v1/service/ModelCheck.go
@@ -5,6 +5,8 @@ import "yssim-go/library/omc"
 func CheckModel(className string) []map[string]string {
 	message := omc.OMC.CheckModel(className)
 	messageList := GetMessagesStringInternal()
-	messageList = append(messageList, map[string]string{"type": "message", "message": message})
+	if message != "" {
+		messageList = append(messageList, map[string]string{"type": "message", "message": message})
+	}
 	return messageList
 }
