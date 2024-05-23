4b233136a7dcc92e3f8bf345d07ce5826157b9f0
zhouqiang <qiang.zhou@simtek.cc>
2024-05-23 16:31:27 +0800

    检查模型产生的日志前面加上模型名称信息

--- app/v1/service/ModelCheck.go
+++ app/v1/service/ModelCheck.go
@@ -8,5 +8,14 @@ func CheckModel(className string) []map[string]string {
 	if message != "" {
 		messageList = append(messageList, map[string]string{"type": "message", "message": message})
 	}
+
+	for _, messageMap := range messageList {
+		msg, ok := messageMap["message"]
+		if !ok {
+			continue
+		}
+		messageMap["message"] = "[" + className + "]: " + msg
+	}
+
 	return messageList
 }
