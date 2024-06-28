6d40e4daad8c14f5e2c3dac3c6ed9deb29baa223
zhouqiang <qiang.zhou@simtek.cc>
2024-06-28 16:15:31 +0800

    优化获取待加载模型列表和加载单个模型接口的代码

--- app/v2/service/ModelLibrary.go
+++ app/v2/service/ModelLibrary.go
@@ -3,6 +3,7 @@ package serviceV2
 import (
 	"log"
 
+	"yssim-go/app/DataBaseModel"
 	serviceV1 "yssim-go/app/v1/service"
 	"yssim-go/library/omc"
 )
@@ -26,4 +27,40 @@ func LibraryInitializationSingle(packageName, packageVersion, filePath string) b
 	ok := omc.OMC.LoadModel(packageName, packageVersion)
 	log.Printf("初始化模型库：%s %s  %t \n", packageName, packageVersion, ok)
 	return ok
+}
+
+// GetAllLoadableModel 获取所有可加载的模型信息
+func GetAllLoadableModel(packageInformation map[string]map[string]string, packageModelAll []DataBaseModel.YssimModels) []map[string]string {
+	data := []map[string]string{}
+
+	if len(packageInformation) == 0 {
+		//packageInformation为空，直接取所有的packageModel信息
+		for _, packageModel := range packageModelAll {
+			m := map[string]string{
+				"id":      packageModel.ID,
+				"name":    packageModel.PackageName,
+				"version": packageModel.Version,
+			}
+			data = append(data, m)
+		}
+	} else {
+		//packageInformation不为空，以packageModelAll为基准取二者交集
+		packageInformationNameMap := map[string]bool{}
+		for packageName := range packageInformation {
+			packageInformationNameMap[packageName] = true
+		}
+
+		for _, packageModel := range packageModelAll {
+			if ok := packageInformationNameMap[packageModel.PackageName]; ok {
+				m := map[string]string{
+					"id":      packageModel.ID,
+					"name":    packageModel.PackageName,
+					"version": packageModel.Version,
+				}
+				data = append(data, m)
+			}
+		}
+	}
+
+	return data
 }
