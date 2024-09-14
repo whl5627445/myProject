2660085a75f8428f429bd991d1fb046d3a98c197
zhouqiang <qiang.zhou@simtek.cc>
2024-09-14 10:04:29 +0800

    映射配置表请求路由路径改为下划线格式

--- router/PipeNetRouter.go
+++ router/PipeNetRouter.go
@@ -9,13 +9,13 @@ import (
 func PipeNetRouter(g *gin.Engine) {
 	var Models = g.Group("/pipenet")
 	{
-		Models.POST("/mappingConfig/upload", API.UploadMappingConfigView)
-		Models.POST("/mappingConfig/download", API.DownloadMappingConfigView)
-		Models.POST("/mappingConfig/delete", API.DeleteMappingConfigView)
-		Models.POST("/mappingConfig/copy", API.CopyMappingConfigView)
-		Models.GET("/mappingConfig/list/get", API.GetMappingConfigListView)
-		Models.POST("/mappingConfig/edit", API.EditMappingConfigView)
-		Models.GET("/mappingConfig/details/get", API.GetMappingConfigDetailsView)
-		Models.POST("/mappingConfig/details/edit", API.EditMappingConfigDetailsView)
+		Models.POST("/mapping_config/upload", API.UploadMappingConfigView)
+		Models.POST("/mapping_config/download", API.DownloadMappingConfigView)
+		Models.POST("/mapping_config/delete", API.DeleteMappingConfigView)
+		Models.POST("/mapping_config/copy", API.CopyMappingConfigView)
+		Models.GET("/mapping_config/list/get", API.GetMappingConfigListView)
+		Models.POST("/mapping_config/edit", API.EditMappingConfigView)
+		Models.GET("/mapping_config/details/get", API.GetMappingConfigDetailsView)
+		Models.POST("/mapping_config/details/edit", API.EditMappingConfigDetailsView)
 	}
 }
