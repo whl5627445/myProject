19a54de7cf3fb73e83db8d93855701aaea5b4d38
simtek/wanghailong <851941017@qq.com>
2025-05-21 16:13:37 +0800

    移除smc连接的gzip压缩命令

--- grpc/SMC/smc.go
+++ grpc/SMC/smc.go
@@ -3,7 +3,6 @@ package smc
 import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
-	"google.golang.org/grpc/encoding/gzip"
 	"google.golang.org/grpc/grpclog"
 	"yssim-go/config"
 )
@@ -12,7 +11,6 @@ func newSMC() SMCClient {
 	conn, err := grpc.NewClient(config.SmcAddr, grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithDefaultCallOptions(
 		grpc.MaxCallSendMsgSize(1024*1024*50),
 		grpc.MaxCallRecvMsgSize(1024*1024*50),
-		grpc.UseCompressor(gzip.Name),
 	))
 	if err != nil {
 		grpclog.Fatalf("did not connect: %v", err)
