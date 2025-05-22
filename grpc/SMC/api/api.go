e752e5cd832acb4c79360089a2584aae8ed67ad0
simtek/wanghailong <851941017@qq.com>
2025-05-22 11:46:50 +0800

    更新smc的gRPC服务端接口注释

--- grpc/SMC/api/api.go
+++ grpc/SMC/api/api.go
@@ -7,6 +7,7 @@ import (
 	smc "yssim-go/grpc/SMC"
 )
 
+// LoadFile 加载指定路径的模型库或者模型
 func LoadFile(path string) (string, error) {
 	req := &smc.LoadFileRequest{
 		Path: path,
@@ -21,6 +22,7 @@ func LoadFile(path string) (string, error) {
 	return result.GetPackageName(), nil
 }
 
+// LoadLibrary 加载modelicaPath下的库，可以指定版本
 func LoadLibrary(packageName, version string) (bool, error) {
 	req := &smc.LoadLibraryRequest{
 		PackageName: packageName,
@@ -36,21 +38,23 @@ func LoadLibrary(packageName, version string) (bool, error) {
 	return result.GetResult(), nil
 }
 
-func GetModelInstance(modelName string) (*smc.ClassDefinition, error) {
-	req := &smc.ModelNameRequest{
-		ModelName: modelName,
+// GetModelInstance 获取模型实例接口
+func GetModelInstance(className string) (*smc.ClassDefinition, error) {
+	req := &smc.ClassNameRequest{
+		ClassName: className,
 	}
 	result, err := smc.SMC.GetModelInstance(context.Background(), req)
 	if err != nil {
 		return nil, err
 	}
 
-	return result.GetModel(), nil
+	return result.GetClass(), nil
 }
 
-func GetModelAST(modelName string) (*smc.ClassDefinition, error) {
-	req := &smc.ModelNameRequest{
-		ModelName: modelName,
+// GetModelAST 获取模型的AST对象
+func GetModelAST(className string) (*smc.ClassDefinition, error) {
+	req := &smc.ClassNameRequest{
+		ClassName: className,
 	}
 	result, err := smc.SMC.GetModelAST(context.Background(), req)
 	if err != nil {
@@ -60,9 +64,10 @@ func GetModelAST(modelName string) (*smc.ClassDefinition, error) {
 	return result.GetModel(), nil
 }
 
-func GetModelCode(modelName string) (string, error) {
-	req := &smc.ModelNameRequest{
-		ModelName: modelName,
+// GetModelCode 获取指定模型源码
+func GetModelCode(className string) (string, error) {
+	req := &smc.ClassNameRequest{
+		ClassName: className,
 	}
 	result, err := smc.SMC.GetModelCode(context.Background(), req)
 	if err != nil {
@@ -72,6 +77,7 @@ func GetModelCode(modelName string) (string, error) {
 	return result.GetCode(), nil
 }
 
+// ParserCode 解析给定源码字符，并指定代码的源路径文件名
 func ParserCode(code, path string) (*smc.ClassDefinition, error) {
 	req := &smc.ParserCodeRequest{
 		Code:       code,
@@ -85,10 +91,11 @@ func ParserCode(code, path string) (*smc.ClassDefinition, error) {
 	return result.GetModel(), nil
 }
 
+// ParserModelCode 解析模型代码，用于模型代码的更新
 func ParserModelCode(code, className string) (*smc.ClassDefinition, error) {
 	req := &smc.ParserModelCodeRequest{
-		ModelCode: code,
-		ModelName: className,
+		ClassCode: code,
+		ClassName: className,
 	}
 	result, err := smc.SMC.ParserModelCode(context.Background(), req)
 	if err != nil {
@@ -97,8 +104,9 @@ func ParserModelCode(code, className string) (*smc.ClassDefinition, error) {
 	return result.GetModel(), nil
 }
 
+// GetClassNames 获取指定模型名称下的子模型名称，可指定是否递归查询，返回子模型名称列表
 func GetClassNames(parentName string, isAll bool) ([]string, error) {
-	req := &smc.ModelNamesRequest{
+	req := &smc.ClassNamesRequest{
 		ParentName: parentName,
 		IsAll:      isAll,
 	}
@@ -109,9 +117,10 @@ func GetClassNames(parentName string, isAll bool) ([]string, error) {
 	return result.GetNameList(), nil
 }
 
+// Save 保存指定模型到SourcePath，更新文件内的代码
 func Save(className string) (bool, error) {
-	req := &smc.ModelNameRequest{
-		ModelName: className,
+	req := &smc.ClassNameRequest{
+		ClassName: className,
 	}
 	result, err := smc.SMC.Save(context.Background(), req)
 	if err != nil {
@@ -120,9 +129,10 @@ func Save(className string) (bool, error) {
 	return result.GetResult(), nil
 }
 
+// GetSubTypeOf 获取指定名称的子类型，可指定筛选继承的父模型
 func GetSubTypeOf(className, parentName string) ([]string, error) {
 	req := &smc.GetSubTypeOfRequest{
-		ModelName:  className,
+		ClassName:  className,
 		ParentName: parentName,
 	}
 	result, err := smc.SMC.GetSubTypeOf(context.Background(), req)
@@ -132,6 +142,7 @@ func GetSubTypeOf(className, parentName string) ([]string, error) {
 	return result.GetSubTypeOfList(), nil
 }
 
+// SetModelicaPath 设置ModelicaPath，用于自动搜索模型库
 func SetModelicaPath(path string) (bool, error) {
 	req := &smc.SetModelicaPathRequest{
 		Path: path,
@@ -143,7 +154,8 @@ func SetModelicaPath(path string) (bool, error) {
 	return result.GetResult(), nil
 }
 
-func GetModelicaPath(path string) ([]string, error) {
+// GetModelicaPath 获取已设置的ModelicaPath
+func GetModelicaPath() ([]string, error) {
 	req := &smc.GetModelicaPathRequest{}
 	result, err := smc.SMC.GetModelicaPath(context.Background(), req)
 	if err != nil {
@@ -152,6 +164,7 @@ func GetModelicaPath(path string) ([]string, error) {
 	return result.GetPathList(), nil
 }
 
+// AddElement 向指定模型添加元素
 func AddElement(e *smc.AddElement) (bool, error) {
 	req := &smc.AddElementRequest{
 		AddElement: e,
@@ -163,6 +176,7 @@ func AddElement(e *smc.AddElement) (bool, error) {
 	return result.GetResult(), nil
 }
 
+// SetElementAnnotation 设置模型元素的注解
 func SetElementAnnotation(annotation *smc.SetElementAnnotation) (bool, error) {
 	req := &smc.SetElementAnnotationRequest{
 		SetElementAnnotation: annotation,
@@ -174,9 +188,10 @@ func SetElementAnnotation(annotation *smc.SetElementAnnotation) (bool, error) {
 	return result.GetResult(), nil
 }
 
+// DeleteElement 删除模型元素
 func DeleteElement(modelName, elementName string) (bool, error) {
 	req := &smc.DeleteElementRequest{
-		ModelName:   modelName,
+		ClassName:   modelName,
 		ElementName: elementName,
 	}
 	result, err := smc.SMC.DeleteElement(context.Background(), req)
@@ -186,6 +201,7 @@ func DeleteElement(modelName, elementName string) (bool, error) {
 	return result.GetResult(), nil
 }
 
+// AddConnection 向指定模型添加连接方程
 func AddConnection(c *smc.AddConnection) (bool, error) {
 	req := &smc.AddConnectionRequest{
 		AddConnection: c,
@@ -197,6 +213,7 @@ func AddConnection(c *smc.AddConnection) (bool, error) {
 	return result.GetResult(), nil
 }
 
+// SetConnectionAnnotation 设置模型方程的注解
 func SetConnectionAnnotation(annotation *smc.SetConnectionAnnotation) (bool, error) {
 	req := &smc.SetConnectionAnnotationRequest{
 		SetConnectionAnnotation: annotation,
@@ -208,9 +225,10 @@ func SetConnectionAnnotation(annotation *smc.SetConnectionAnnotation) (bool, err
 	return result.GetResult(), nil
 }
 
-func DeleteConnection(modelName, connectStart, connectEnd string) (bool, error) {
+// DeleteConnection 删除模型方程
+func DeleteConnection(className, connectStart, connectEnd string) (bool, error) {
 	req := &smc.DeleteConnectionRequest{
-		ModelName: modelName,
+		ClassName: className,
 		Left:      connectStart,
 		Right:     connectEnd,
 	}
@@ -221,10 +239,11 @@ func DeleteConnection(modelName, connectStart, connectEnd string) (bool, error) 
 	return result.GetResult(), nil
 }
 
-func SetClassAnnotation(modelName string, annotation *smc.Argument) (bool, error) {
+// SetClassAnnotation 设置类注释
+func SetClassAnnotation(className string, annotation *smc.Argument) (bool, error) {
 	req := &smc.SetClassAnnotationRequest{
 		SetClassAnnotation: annotation,
-		ModelName:          modelName,
+		ClassName:          className,
 	}
 	result, err := smc.SMC.SetClassAnnotation(context.Background(), req)
 	if err != nil {
